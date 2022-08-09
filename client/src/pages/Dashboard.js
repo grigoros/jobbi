0d467af6f227b98a87368de4f79dd75225aa8e02
Justin Mills <justinamills@gmail.com>
2022-08-08 20:30:56 -0400

    adding user model, basic server fx, and cors fix

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,10 +1,23 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 const Dashboard = () => {
+  const fetchData = async () => {
+    try {
+      const response = await fetch('/api/v1')
+      const data = await response.json()
+      console.log(data)
+    } 
+    catch (error) {
+        console.log(error)
+    }
+  }  
+  useEffect(()=> {
+    fetchData()
+  }, [])
   return (
-    <div>
-      DashBoard
-    </div>
+    <h1>
+      Dashboard
+    </h1>
   )
 }
 
--- server.js
+++ server.js
@@ -18,8 +18,11 @@ import errorHandlerMiddleware from './middleware/error-handler.js'
 app.use(express.json())
 
 app.get('/', (req, res) => {
-  throw new Error('error')
-  res.send('Welcome!')
+  res.json('Welcome!')
+})
+
+app.get('/api/v1', (req, res) => {
+  res.json('API')
 })
 
 app.use('/api/v1/auth', authRouter)
