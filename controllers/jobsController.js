968ad2346cdac314d57a452573d6e51889f35d47
Justin Mills <justinamills@gmail.com>
2022-08-14 15:32:21 -0400

    delete function fixed

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -1,6 +1,8 @@
 import Job from "../models/Job.js"
 import { StatusCodes } from "http-status-codes"
 import { BadRequestError, NotFoundError, UnauthenticatedError } from "../errors/index.js"
+import CustomAPIError from "../errors/custom-api.js"
+import checkPermissions from "../utils/checkPermissions.js"
 
 const createJob = async (req, res) => {
   const { position, company } = req.body
@@ -33,6 +35,8 @@ const updateJob = async (req, res) => {
   }
 
   // check permissions
+  checkPermissions(req.user, job.createdBy)
+
   const updatedJob = await Job.findOneAndUpdate({ _id: jobId }, req.body, {
     new: true,
     runValidators: true,
@@ -42,7 +46,16 @@ const updateJob = async (req, res) => {
 }
 
 const deleteJob = async (req, res) => {
-  res.send('delete job')
+  const { id: jobId } = req.params
+  const job = await Job.findOne({ _id: jobId })
+
+  if (!job) {
+    throw new NotFoundError(`No job with id: ${jobId}`)
+  }
+
+  checkPermissions(req.user, job.createdBy)
+  await job.remove()
+  res.status(StatusCodes.OK).json({msg: 'Success! job removed' })
 }
 
 const showStats = async (req, res) => {
